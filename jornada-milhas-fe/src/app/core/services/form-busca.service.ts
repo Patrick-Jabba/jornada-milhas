b41ca636f478da4da93a252a97f0c0ccc4e87677
Patrick Monteiro Fischer <patrickmf.dsn.pack@alterdata.matriz>
2023-10-06 17:30:09 -0300

    :recycle: refatorando o modal para alternar os valores de categoria de passagem dinamicamente

--- jornada-milhas-fe/src/app/core/services/form-busca.service.ts
+++ jornada-milhas-fe/src/app/core/services/form-busca.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { FormBusca } from '../types/types';
+import { ModalComponent } from 'src/app/shared/modal/modal.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MatChipSelectionChange } from '@angular/material/chips';
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +11,13 @@ import { FormBusca } from '../types/types';
 export class FormBuscaService {
   formBusca: FormGroup<FormBusca>;
 
-  constructor() {
+  constructor(private dialog: MatDialog) {
     this.formBusca = new FormGroup<FormBusca>({
       somenteIda: new FormControl(false),
       adultos: new FormControl(1),
       criancas: new FormControl(0),
       bebes: new FormControl(0),
-      tipo: new FormControl('Econômica'),
+      tipo: new FormControl('Executiva'),
       origem: new FormControl(),
       destino: new FormControl(),
       dataIda: new FormControl(),
@@ -29,5 +32,21 @@ export class FormBuscaService {
     }
 
     return control as FormControl;
+  }
+
+  alterarTipo(evento: MatChipSelectionChange, tipo: string) {
+    if(evento.selected){
+      this.formBusca.patchValue({
+        tipo
+      })
+    }
+
+    console.log(tipo)
+  }
+
+  openDialog() {
+    this.dialog.open(ModalComponent, {
+      width: '50%'
+    });
   }
 }
